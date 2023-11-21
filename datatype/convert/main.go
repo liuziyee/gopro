4b7151f97b4043fad288c92313ea988180f119bc
dorohedoro <liuziyee1994@163.com>
2023-11-21 17:31:30 +0800

    :iphone:

--- datatype/convert/main.go
+++ datatype/convert/main.go
@@ -17,12 +17,34 @@ func main() {
 	fmt.Println(e)
 
 	var str = "12"
-	atoi, err := strconv.Atoi(str) // 字符串转数字
+	atoi, err := strconv.Atoi(str) // 字符串转整型
 	if err != nil {
 		fmt.Println("error")
 	}
 	fmt.Println(atoi)
 
 	var in = 0
-	fmt.Println(strconv.Itoa(in)) // 数字转字符串
+	fmt.Println(strconv.Itoa(in)) // 整型转字符串
+
+	// parsexxx:字符串转基本类型
+	f64, err := strconv.ParseFloat("3.14", 64) // 字符串转浮点数
+	if err != nil {
+		fmt.Println("error")
+	}
+	fmt.Println(f64)
+
+	in64, err := strconv.ParseInt("30", 8, 64) // 字符串转整型
+	fmt.Println(in64)
+
+	boo, err := strconv.ParseBool("0")
+	if err != nil {
+		fmt.Println("error")
+	}
+	fmt.Println(boo)
+
+	// formatxxx:基本类型转字符串
+	formatBool := strconv.FormatBool(true)
+	fmt.Println(formatBool)
+
+	fmt.Println(strconv.FormatFloat(3.1415, 'f', -1, 64))
 }
